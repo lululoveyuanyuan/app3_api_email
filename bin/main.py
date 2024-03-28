e31657ea8e13dfc1bbf715268bc9b43e22b16d6d
lulu <xu.lulu@northeastern.edu>
2024-03-28 14:52:27 -0400

    code modified to get a better email format

--- bin/main.py
+++ bin/main.py
@@ -1,10 +1,20 @@
 import requests
+from send_email import send_email
 
 api_key = "6dfffb3abadd48baa056f83dbca0e4e6"
 url = "https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=6dfffb3abadd48baa056f83dbca0e4e6"
 
+# Make request
 request = requests.get(url)
+
+# Get a dictionary with data
 content = request.json()
+
+# Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["descriptions"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
